2b6bde09e6d9f592ab264d17249cf7360fa974a4
Julilib <121502247+Julilib@users.noreply.github.com>
2023-01-04 17:15:36 +0100

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -4,60 +4,54 @@ import vue from '@vitejs/plugin-vue2';
 import htmlTemplate from 'vite-plugin-html-template';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  process.env = { ...process.env, ...loadEnv(process.cwd(), '') };
-  const app = process.env.VITE_APP;
-
-  return {
-    plugins: [
-      vue(),
-      htmlTemplate.default({
-        pages: {
-          index: {
-            template: 'public/index.html',
-            entry: 'src/main.js',
-          },
-          logout: {
-            entry: 'src/main.js',
-          },
+export default defineConfig({
+  plugins: [
+    vue(),
+    htmlTemplate.default({
+      pages: {
+        index: {
+          template: 'public/index.html',
+          entry: 'src/main.js',
         },
-      }),
-    ],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: () => {
-            let prepends = '';
-            prepends += `$app: ${app};`;
-            prepends += `@import "@/style.scss";`;
-            prepends += `@import "@/assets/vite/scss/colors.scss";`;
-            // prepends += `@import "@/components/HelloWorld.scss";`; //Here will load it
-            return prepends;
-          },
+        logout: {
+          entry: 'src/main.js',
+        },
+      },
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: () => {
+          let prepends = '';
+          prepends += `@import "@/style.scss";`;
+          prepends += `@import "@/assets/vite/scss/colors.scss";`;
+          // prepends += `@import "@/components/HelloWorld.scss";`; //Here will load it
+          return prepends;
         },
       },
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        homepage: 'public/index.html',
+        logout: 'public/logout.html',
+      },
+      output: {
+        entryFileNames: '[name].bundle.js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
       },
     },
-    build: {
-      rollupOptions: {
-        input: {
-          homepage: 'public/index.html',
-          logout: 'public/logout.html',
-        },
-        output: {
-          entryFileNames: '[name].bundle.js',
-          chunkFileNames: '[name].js',
-          assetFileNames: '[name].[ext]',
-        },
-      },
-      outDir: path.resolve(__dirname, 'dist/' + app),
-      sourcemap: true,
-      emptyOutDir: true,
-      minify: true,
-    },
-  };
+    outDir: path.resolve(__dirname, 'dist/'),
+    sourcemap: true,
+    emptyOutDir: true,
+    minify: true,
+  },
 });
